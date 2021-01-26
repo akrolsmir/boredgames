59ef2f3989e5f00981992fb23db81814bc5e00b1
Austin Chen <akrolsmir@gmail.com>
2021-01-26 12:52:48 -0500

    Extract firebase logic

--- src/components/GamesTable.vue
+++ src/components/GamesTable.vue
@@ -25,43 +25,7 @@
 
 <script>
 import GameCard from './GameCard.vue'
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyBOe0NYQsCsDTe1rXutCksl41yWQLfNiT8',
-  authDomain: 'boredgames-e54c6.firebaseapp.com',
-  projectId: 'boredgames-e54c6',
-  storageBucket: 'boredgames-e54c6.appspot.com',
-  messagingSenderId: '822062307615',
-  appId: '1:822062307615:web:4e4fdee4d33520862cf82a',
-  measurementId: 'G-S3RLG829D7',
-}
-// Initialize Firebase
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig)
-  try {
-    firebase.analytics()
-  } catch (e) {
-    console.warn('Firebase analytics not enabled (probably got blocked.)')
-    // Shim for firebase.analytics().logEvent(...)
-    firebase.analytics = () => ({
-      logEvent() {
-        // Do nothing
-      },
-    })
-  }
-}
-const db = firebase.firestore()
-const COLLECTION = 'games'
-
-function sanitize(text) {
-  return text
-    .trim()
-    .toLowerCase()
-    .replace(/\s/g, '-') // whitespace
-    .replace(/[^-\d\p{L}]/gu, '') // not (dash or number or letter in any language)
-}
+import { loadTable, addGame } from '../firebase-network.js'
 
 export default {
   components: {
@@ -87,20 +51,8 @@ export default {
     async newGame() {
       await this.addGame(this.newTitle, this.newUrl)
     },
-    async loadTable() {
-      const docs = await db.collection(COLLECTION).get()
-
-      const games = []
-      docs.forEach((doc) => games.push(doc.data()))
-      return games
-    },
-    async addGame(title, url) {
-      const id = sanitize(title)
-      await db.collection(COLLECTION).doc(id).set({
-        title,
-        url,
-      })
-    },
+    loadTable,
+    addGame,
   },
 }
 </script>
---
+++ src/firebase-network.js
@@ -0,0 +1,55 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBOe0NYQsCsDTe1rXutCksl41yWQLfNiT8',
+  authDomain: 'boredgames-e54c6.firebaseapp.com',
+  projectId: 'boredgames-e54c6',
+  storageBucket: 'boredgames-e54c6.appspot.com',
+  messagingSenderId: '822062307615',
+  appId: '1:822062307615:web:4e4fdee4d33520862cf82a',
+  measurementId: 'G-S3RLG829D7',
+}
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+  try {
+    firebase.analytics()
+  } catch (e) {
+    console.warn('Firebase analytics not enabled (probably got blocked.)')
+    // Shim for firebase.analytics().logEvent(...)
+    // TODO: Move logEvent into a generally logger here, which catches errors
+    firebase.analytics = () => ({
+      logEvent() {
+        // Do nothing
+      },
+    })
+  }
+}
+const db = firebase.firestore()
+const COLLECTION = 'games'
+
+export async function loadTable() {
+  const docs = await db.collection(COLLECTION).get()
+
+  const games = []
+  docs.forEach((doc) => games.push(doc.data()))
+  return games
+}
+
+export async function addGame(title, url) {
+  const id = sanitize(title)
+  await db.collection(COLLECTION).doc(id).set({
+    title,
+    url,
+  })
+}
+
+// TODO: Extract to utils
+function sanitize(text) {
+  return text
+    .trim()
+    .toLowerCase()
+    .replace(/\s/g, '-') // whitespace
+    .replace(/[^-\d\p{L}]/gu, '') // not (dash or number or letter in any language)
+}

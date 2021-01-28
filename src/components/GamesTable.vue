37c6e4c8e2f4a27feea4d0f03429a9b790630f0c
Austin Chen <akrolsmir@gmail.com>
2021-01-27 20:16:33 -0500

    FIlter games by tags, players, and max playtime

--- src/components/GamesTable.vue
+++ src/components/GamesTable.vue
@@ -1,10 +1,51 @@
 <template>
   <h1 class="title">The Big List of Online Board Games</h1>
+
+  <div class="field is-horizontal">
+    <div class="field-label is-normal">
+      <label class="label">Search for a game</label>
+    </div>
+    <div class="field-body">
+      <div class="field">
+        <p class="control">
+          <input
+            class="input"
+            type="text"
+            v-model="filter.tags"
+            placeholder="Tags"
+          />
+        </p>
+      </div>
+      <div class="field">
+        <p class="control">
+          <input
+            class="input"
+            type="number"
+            v-model="filter.players"
+            placeholder="# players"
+          />
+        </p>
+      </div>
+      <div class="field">
+        <p class="control">
+          <input
+            class="input"
+            type="number"
+            v-model="filter.maxPlaytime"
+            placeholder="Max playtime"
+          />
+        </p>
+      </div>
+    </div>
+  </div>
+  <br /><br />
+
   <div class="columns">
     <div class="column" v-for="col in table">
       <GameCard v-for="game in col" :game="game" @edit="toEdit = game" />
     </div>
   </div>
+
   <div class="columns">
     <div class="column">
       <h2 class="title is-4">Submit a game!</h2>
@@ -15,6 +56,7 @@
       <GameCard :game="toEdit" />
     </div>
   </div>
+
   <GamesList :games="games" />
 </template>
 
@@ -51,6 +93,11 @@ export default {
       newTitle: '',
       newUrl: '',
       games: [],
+      filter: {
+        tags: undefined,
+        players: undefined,
+        maxPlaytime: undefined,
+      },
     }
   },
   async created() {
@@ -61,16 +108,44 @@ export default {
     async reload() {
       this.games = await this.loadTable()
     },
+    filterFunction(game) {
+      if (this.filter.players) {
+        if (
+          game.attr.minPlayers > this.filter.players ||
+          this.filter.players > game.attr.maxPlayers
+        ) {
+          return false
+        }
+      }
+      if (this.filter.maxPlaytime) {
+        if (this.filter.maxPlaytime < game.attr.playtime) {
+          return false
+        }
+      }
+      if (this.filter.tags) {
+        function match(tag, query) {
+          return tag.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+        }
+        const anyMatch = game.tags.some((tag) => match(tag, this.filter.tags))
+        if (!anyMatch) {
+          return false
+        }
+      }
+      return true
+    },
   },
   computed: {
     table() {
       const numColumns = 3
       // Array().fill([]) doesn't work since the array ref is the same
       const result = Array.from(new Array(numColumns), () => [])
-      for (const [i, game] of this.games.entries()) {
+      for (const [i, game] of this.filteredGames.entries()) {
         result[i % 3].push(game)
       }
       return result
+    },
+    filteredGames() {
+      return this.games.filter(this.filterFunction)
     },
   },
 }
